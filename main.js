e09ae52b4724a4ebba2fa839198c1f272c184c40
luisjarq <lc.luisja@gmail.com>
2021-12-09 22:04:36 +0100

    updated cors policy

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // Middlewares importacion
 const express = require("express");
-const moviesRouter = require('./routers/movies.router')
+const moviesRouter = require("./routers/movies.router");
+const cors = require("cors");
 // Variables
 const server = express();
-const { PORT } = require('./config/config');
-const { DB_URL } = require('./db/db_atlas');
+const { PORT } = require("./config/config");
+const { DB_URL } = require("./db/db_atlas");
 // Middlewares para entender los json bodys
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+server.use(cors());
 // Middleware enrutador
-server.use('/movies', moviesRouter);
+server.use("/movies", moviesRouter);
 // Middleware de enrutado para rutas no existentes
 server.use("*", (req, res, next) => {
   const error = new Error("Ruta no encontrada");
@@ -17,7 +19,7 @@ server.use("*", (req, res, next) => {
   next(error);
 });
 // Manejador/Middleware de errores, siempre se define con los 4 parametros (err, req, res, next)
-server.use((error,req, res, next ) => {
+server.use((error, req, res, next) => {
   console.error("[ERROR] Ha ocurrido un error", error.status, error.message);
   res
     .status(error.status || 500)
